4cd10e2324ed7dbabab8c8fac3ccaf6a6f7cad78
11 <11@11.com>
2019-03-08 17:32:15 +0800

    httpClient

---
+++ JavaHttpClient/src/main/java/com/course/HttpGetTest.java
@@ -0,0 +1,49 @@
+package com.course;
+
+
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpGetTest {
+
+    public static void main(String[] args) {
+
+        //创建HttpClient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        //创建HttpGet对象，设置URL访问地址
+        HttpGet httpGet = new HttpGet("http://www.itcast.com");
+        CloseableHttpResponse response = null;
+        //使用HttpClient发起请求，获取response
+        try {
+             response= httpClient.execute(httpGet);
+
+            //解析响应
+            if(response.getStatusLine().getStatusCode() == 200){
+                String content = EntityUtils.toString(response.getEntity(), "utf-8");
+                System.out.println(content.length());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            //关闭reponse
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //关闭httpClient
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
